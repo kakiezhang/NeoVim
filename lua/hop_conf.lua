ef69f319b84b3b55cff71b4ef54202b4cb2f51dd
kakie <782922086@qq.com>
2022-04-20 17:53:47 +0800

    hop visual mode

--- lua/hop_conf.lua
+++ lua/hop_conf.lua
@@ -15,16 +15,34 @@ hop.setup {
 	jump_on_sole_occurrence = false, 
 }
 
+-- NormalMode
+
 -- Move to Word
-vim.api.nvim_set_keymap('n', 'mw', "<cmd>lua require'hop'.hint_words()<cr>", {noremap = true})
+vim.api.nvim_set_keymap('n', 'mw', "<cmd>lua require'hop'.hint_words()<cr>", {noremap = true, silent = true})
 -- Move to Line
-vim.api.nvim_set_keymap('n', 'mL', "<cmd>lua require'hop'.hint_lines()<cr>", {noremap = true})
+vim.api.nvim_set_keymap('n', 'mL', "<cmd>lua require'hop'.hint_lines()<cr>", {noremap = true, silent = true})
 
 -- Move to Left
-vim.api.nvim_set_keymap('n', 'mh', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {noremap = true})
+vim.api.nvim_set_keymap('n', 'mh', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {noremap = true, silent = true})
 -- Move to Right
-vim.api.nvim_set_keymap('n', 'ml', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {noremap = true})
+vim.api.nvim_set_keymap('n', 'ml', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {noremap = true, silent = true})
 -- Move to Up
-vim.api.nvim_set_keymap('n', 'mk', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<cr>", {noremap = true})
+vim.api.nvim_set_keymap('n', 'mk', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<cr>", {noremap = true, silent = true})
 -- Move to Down
-vim.api.nvim_set_keymap('n', 'mj', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<cr>", {noremap = true})
+vim.api.nvim_set_keymap('n', 'mj', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<cr>", {noremap = true, silent = true})
+
+-- VisalMode
+
+-- Move to Word
+vim.api.nvim_set_keymap('v', 'mw', "<cmd>lua require'hop'.hint_words()<cr>", {noremap = true, silent = true})
+-- Move to Line
+vim.api.nvim_set_keymap('v', 'mL', "<cmd>lua require'hop'.hint_lines()<cr>", {noremap = true, silent = true})
+
+-- Move to Left
+vim.api.nvim_set_keymap('v', 'mh', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {noremap = true, silent = true})
+-- Move to Right
+vim.api.nvim_set_keymap('v', 'ml', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {noremap = true, silent = true})
+-- Move to Up
+vim.api.nvim_set_keymap('v', 'mk', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<cr>", {noremap = true, silent = true})
+-- Move to Down
+vim.api.nvim_set_keymap('v', 'mj', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<cr>", {noremap = true, silent = true})
